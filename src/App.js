373909bbf396eadd2a80c3193b01a8ce46259c89
Adalberto <adalberto.cuevas@gmail.com>
2019-04-28 11:41:53 -0400

    No finish yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Nav from "./component/layout/Nav/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import JumboBox from "./component/layout/Search/JumboBox";
 import Search from "./component/layout/Search/Search";
 import Web3 from "aion-web3";
 
 export default class App extends Component {
   state = {
+    account: "",
     temp: "",
     humidity: "",
     waterPresure: "",
@@ -52,53 +54,100 @@ export default class App extends Component {
     setInterval(this.getFunction, 1000);
   }
 
-  // //send transaction to the smart contract
-  // sendTransactionFunction = async mystring => {
-  //   //set web3
-  //   let web3 = new Web3(
-  //     new Web3.providers.HttpProvider(this.state.httpProvider)
-  //   );
+  //send transaction to the smart contract
+  sendTransactionFunction = async mystring => {
+    //set web3
+    let web3 = new Web3(
+      new Web3.providers.HttpProvider(this.state.httpProvider)
+    );
 
-  //   //set aiwa accouunt
-  //   try {
-  //     this.setState({
-  //       account: window.aionweb3.account[0]
-  //     });
-  //   } catch (e) {
-  //     console.error("no account for sending", e.message);
-  //   }
+    //set aiwa accouunt
+    try {
+      this.setState({
+        account: window.aionweb3.account[0]
+      });
+    } catch (e) {
+      console.error("no account for sending", e.message);
+    }
 
-  //   //the contract method you want to call
-  //   let data = web3.avm.contract
-  //     .method("setString")
-  //     .inputs(["string"], [mystring])
-  //     .encode();
+    //the contract method you want to call
+    let data = web3.avm.contract
+      .method("setString")
+      .inputs(["string"], [mystring])
+      .encode();
 
-  //   const txObject = {
-  //     from: this.state.account,
-  //     to: this.state.ctAddress,
-  //     data: data,
-  //     gas: 2000000,
+    const txObject = {
+      from: this.state.account,
+      to: this.state.ctAddress,
+      data: data,
+      gas: 2000000,
 
-  //     type: "0x1" //for any transaction except for java contract deployment
-  //   };
+      type: "0x1" //for any transaction except for java contract deployment
+    };
 
-  //   try {
-  //     let txHash = await window.aionweb3
-  //       .sendTransaction(txObject)
-  //       .then(function(txHash) {
-  //         console.log("txHash:", txHash);
-  //       });
+    try {
+      let txHash = await window.aionweb3
+        .sendTransaction(txObject)
+        .then(function(txHash) {
+          console.log("txHash:", txHash);
+        });
 
-  //     setInterval(async function() {
-  //       const receipt = await web3.eth
-  //         .getTransactionReceipt(txHash)
-  //         .then(console.log);
-  //     }, 30000);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+      setInterval(async function() {
+        const receipt = await web3.eth
+          .getTransactionReceipt(txHash)
+          .then(console.log);
+      }, 30000);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //call smart contract method
+  getFunction = async () => {
+    let web3 = new Web3(
+      new Web3.providers.HttpProvider(this.state.httpProvider)
+    );
+
+    //set aiwa accouunt
+    try {
+      this.setState({
+        account: window.aionweb3.account[0]
+      });
+    } catch (e) {
+      console.error("no account for sending", e.message);
+    }
+
+    let data = web3.avm.contract.method("getId()").encode();
+
+    let txObject = {
+      from: this.account,
+      to: this.state.ctAddress,
+      gas: 100000,
+      gasPrice: 1,
+      data: data
+    };
+
+    try {
+      let res = await web3.eth.call(txObject); //call a method
+      let returnValue = await web3.avm.contract.decode("string", res);
+      this.setState({
+        result: returnValue
+      });
+    } catch (err) {
+      console.log("fail calling");
+    }
+  };
+
+  //get user input
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = event => {
+    //alert('A name was submitted: ' + this.state.value);
+    this.sendTransactionFunction(this.state.value);
+    event.preventDefault();
+  };
 
   componentWillUpdate(nextProps, nexState) {
     localStorage.setItem("tempUpdate", JSON.stringify(nexState.temp));
@@ -107,11 +156,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <Nav />
-        <JumboBox />
-        <Search />
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <JumboBox />
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Search} />
+            {/* <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} /> */}
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/component/layout/Search/Search.js
+++ src/component/layout/Search/Search.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import "./search.css";
+import SearchField from "react-search-field";
 
 export default class Search extends Component {
+  state = {};
+
   render() {
-    return <div>Testing</div>;
+    return (
+      <div>
+        <h2>Search Properties</h2>
+        <SearchField
+          placeholder="Search..."
+          //   onChange={onChange}s
+          searchText="3345 Dundas street west"
+          classNames="test-class"
+        />
+        
+      </div>
+    );
   }
 }
